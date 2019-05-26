ab578bdb4498bca1a932f09fcc5f9129b116c738
Kalidomra <40541205+Kalidomra@users.noreply.github.com>
2019-05-26 16:28:14 -0600

    Delete ResetInterrupt.ino

--- AdamNet_Drive_Emulator/ResetInterrupt.ino
+++
@@ -1,5 +0,0 @@
-void ResetInterrupt(){                                             // Reset from the Adam  
-  TimetoByte = millis();                   // Set the time that the reset came in
-  ResetFlag = 1;                           // Set the reset flag
-  EIFR = bit (INTF1);                      // Clear flag for any interrupts on INT1 that were triggered while we were in the ISR
-}
